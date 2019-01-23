7ff370f4aefbcd233ca92ce4d730fe1e67db02c6
Catherine Korovkina <c.korovkina@gmail.com>
2019-01-23 13:46:56 +0300

    Initial Commit

---
+++ Look!Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Look!Image
+//
+//  Created by Catherine Korovkina on 23/1/2562 BE.
+//  Copyright © 2562 snm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
